86473468ab9c439d69117d86b1de24d87f54b127
Al'bert <bachkovalbert@mail.ru>
2022-06-18 19:21:54 +0500

    CHANGES: +task13?

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+/*
+
 Console.Clear();
 
 int Function(int Num)
@@ -18,7 +20,7 @@ int Function(int Num)
     }
 }
 
-Console.Write($"Input a three-digit number: ");
+Console.Write($"Input a integer three-digit number: ");
 int Num = Convert.ToInt32(Console.ReadLine());
 
 int result = Function(Num);
@@ -32,3 +34,47 @@ else
     Console.WriteLine($"Second digit is {result}");
 }
 
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+///*
+
+Console.Clear();
+
+int Function(int Num)
+{
+    if(Num > 100 && Num > 10)
+    {
+        return Num;
+    }
+    else
+    {
+        return -1; 
+    }
+}
+
+Console.Write($"Input a integer positive number: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int result = Function(Num);
+string NumT = Convert.ToString(result);
+
+if(result == -1)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    Console.WriteLine($"Third digit is {NumT[2]}");
+}
+
+//*/
+
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+
+*/
