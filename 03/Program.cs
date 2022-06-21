401fb623f759ff9f0218f8b3b90410d0aef480b8
Al'bert <bachkovalbert@mail.ru>
2022-06-21 14:07:46 +0500

    CHANGES: +task25

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+void Function(double A, double B)
+{
+    double result = Math.Pow(A,B);
+    Console.Write(result);
+}
+ 
+Console.Write("Input first number: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input second number: ");
+double B = Convert.ToDouble(Console.ReadLine());
+
+Function(A,B);
+
+
+//Function(A, B);
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19, 3, 44, 3 -> [1, 2, 5, 7, 19, 3, 44, 3]
