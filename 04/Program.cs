a3366aca42534f0819d75599cdac6104286af8ba
Al'bert <bachkovalbert@mail.ru>
2022-06-21 14:33:03 +0500

    CHANGES: +04 and 03->04

---
+++ 04/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+/*
+
+Console.Clear();
+
+void Function(double A, double B)
+{
+    double result = Math.Pow(A,B);
+    Console.Write(result);
+}
+ 
+Console.Write("Input first number: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input second number: ");
+double B = Convert.ToDouble(Console.ReadLine());
+
+Function(A,B);
+
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19, 3, 44, 3 -> [1, 2, 5, 7, 19, 3, 44, 3]
+
+Console.Clear();
