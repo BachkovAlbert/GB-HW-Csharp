825a22f4d3b9ccdf7f1781e98512dbaf20f2ab04
Al'bert <bachkovalbert@mail.ru>
2022-06-27 10:23:40 +0500

    CHANGES: +06(41)

--- 06/Program.cs
+++ 06/Program.cs
@@ -1,5 +1,63 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+
+/*
+
+Console.Clear();
+
+Console.Write("Input number of digits(M): ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] ArrayNumbers = new int[M];
+
+void InputNumbers(int M)
+{
+for (int i = 0; i < M; i++)
+  {
+    Console.Write($"Input number {i+1} : ");
+    ArrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int FindNumbersMoreZero(int[] ArrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < ArrayNumbers.Length; i++)
+  {
+    if(ArrayNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(M);
+
+Console.WriteLine($"Numbers > 0 -> {FindNumbersMoreZero(ArrayNumbers)} ");
+
+*/
 
 // Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+///*
+
+Console.Clear();
+
+Console.Write("Input number of digits(M): ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] ArrayNumbers = new int[M];
+
+void InputNumbers(int M)
+{
+for (int i = 0; i < M; i++)
+    {
+        Console.Write($"Input number {i+1} : ");
+        ArrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+for (int i = 0; i < ArrayNumbers.Length; i++)
+    {
+        Console.Write(ArrayNumbers[i] + ", ");
+    }
+}
+
+InputNumbers(M);
+
+
+//*/
