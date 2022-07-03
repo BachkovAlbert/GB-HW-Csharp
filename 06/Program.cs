c1c13c0ca3218ad73a089abee4fb8f75c1479ba6
Al'bert <bachkovalbert@mail.ru>
2022-07-03 15:47:47 +0500

    CHANGES: +06(45)

--- 06/Program.cs
+++ 06/Program.cs
@@ -35,29 +35,40 @@ Console.WriteLine($"Numbers > 0 -> {FindNumbersMoreZero(ArrayNumbers)} ");
 
 // Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
 
-///*
+/*
 
 Console.Clear();
 
-Console.Write("Input number of digits(M): ");
-int M = Convert.ToInt32(Console.ReadLine());
-int[] ArrayNumbers = new int[M];
+Console.Write("Input number of digits(Size): ");
+int Size = Convert.ToInt32(Console.ReadLine());
+int[] ArrayNumbers = new int[Size];
 
-void InputNumbers(int M)
+void InputNumbers(int Size)
 {
-for (int i = 0; i < M; i++)
+for (int i = 0; i < Size; i++)
     {
         Console.Write($"Input number {i+1} : ");
         ArrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
     }
-
-for (int i = 0; i < ArrayNumbers.Length; i++)
-    {
-        Console.Write(ArrayNumbers[i] + ", ");
-    }
 }
 
-InputNumbers(M);
+int[] CopyArray(int[] array)
+{
+  int[] NewArray = new int[array.Length];
+  for (int i = 0; i < array.Length; i++)
+  {
+    NewArray[i] = array[i];
+  }
+  return NewArray;
+}
 
+InputNumbers(Size);
+int[] CopArray = CopyArray(ArrayNumbers);
 
-//*/
+Console.WriteLine("Copy your array:");
+for(int i = 0; i < CopArray.Length; i++)
+{
+  Console.WriteLine(CopArray[i] + " ");
+}
+
+*/
