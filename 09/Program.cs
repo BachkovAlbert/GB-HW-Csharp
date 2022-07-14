ec5561f7a2f17e95a4603432a55eb5845a199259
Albert <bachkovalbert@mail.ru>
2022-07-14 20:39:04 +0500

    CHANGES: +09(66+67)

--- 09/Program.cs
+++ 09/Program.cs
@@ -1,7 +1,41 @@
 ﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-//M = 1; N = 15 -> 120
-//M = 4; N = 8. -> 30
+
+/*
+
+Console.Clear();
+
+int SumNums(int M, int N)
+{
+    if(M == N) return M;
+    if(M > N) return M + (SumNums(M-1,N)); 
+    return N + (SumNums(M,N-1));
+}
+
+Console.Write("Input integer number(M): ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input integer number(N): ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"The sum of the numbers in the gap is {SumNums(M, N)}");
+
+*/
 
 //Задача 67: Напишите программу, которая будет принимать на вход число и возвращать кол-во его цифр.
-//453 -> 3
-//45 -> 2
+
+/*
+Console.Clear();
+
+int NumberOfDigits(int Num)
+{
+    if(Num == 0) return 0;
+    return  1 + NumberOfDigits(Num/10);
+}
+
+Console.Write("Input integer number(Num): ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{NumberOfDigits(Num)}");
+*/
+
+
+
